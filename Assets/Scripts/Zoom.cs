fa06873b7ca7a96de0f24fed3a9fd25a46d93dc0
Arquery <sgu_sigu@msn.com>
2019-02-18 11:43:48 -0700

    Upload Project

---
+++ Assets/Scripts/Zoom.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoom : MonoBehaviour {
+
+	public int zoom = 20;
+	public int normal = 60;
+	public float smooth = 3.0f;
+
+	private bool isZoomed = false;
+
+	void update(){
+		if(Input.GetMouseButtonDown(1)){
+			isZoomed = !isZoomed;
+		}
+
+		if(isZoomed){
+			GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, zoom, Time.deltaTime * smooth);
+		}
+		else{
+			GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, normal, Time.deltaTime * smooth);
+		}
+	} 
+}

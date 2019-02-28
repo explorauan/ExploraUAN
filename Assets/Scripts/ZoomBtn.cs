ea763aa685ba6f406ac4b4a01217d3f9f819d750
Arquery <sgu_sigu@msn.com>
2019-02-28 11:44:31 -0700

    Zoom in
    He was created a C# script called ZoomBtn to augmented the size from an object. The script is found on the directory Assets/Scripts/
    Line comments:
    
    //Scale variable
    //Variables from speed scale and status of scale
    //This code line augmented the y's position from gameobject

---
+++ Assets/Scripts/ZoomBtn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomBtn : MonoBehaviour {
+
+	//Scale variable
+	public GameObject scale;
+
+	//Variables from speed scale n' status of scale
+	public float scaleSpeed = 0.01f;
+	bool scaleStatus = false;
+
+	public void Scalation(){
+		if(scaleStatus==false){
+			scaleStatus = true;
+		}
+		else{
+			scaleStatus = false;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(scaleStatus==true){
+			//This code line augmented the y's position from gameobject
+			scale.transform.localPosition += new Vector3(0, scaleSpeed, 0);
+		}
+	}
+}
---
+++ Assets/Scripts/ZoomBtnmen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomBtnmen : MonoBehaviour {
+	
+//Scale variable
+	public GameObject scale;
+
+	//Variables from speed scale n' status of scale
+	public float scaleSpeed = 0.01f;
+	bool scaleStatus = false;
+
+	public void Scalation(){
+		if(scaleStatus==false){
+			scaleStatus = true;
+		}
+		else{
+			scaleStatus = false;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(scaleStatus==true){
+			scale.transform.localPosition -= new Vector3(0, scaleSpeed, 0);
+		}
+	}
+}

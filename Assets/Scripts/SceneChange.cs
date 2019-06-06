8a4ebbeacd03222964c4b4ac3e2081d703651825
Arquery <sgu_sigu@msn.com>
2019-06-05 22:35:09 -0600

    Last changes
    Inside last changes were added the rest models and imagetargets. The namescene where save the models has change to "Camara". Were created two scenes, one called "Inicio and other called "Pantalla principal, are located in the Assets\ directory. Was added an script called "SceneChange" located in Assets\Scripts\ directory.

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class SceneChange : MonoBehaviour {
+
+	public void MainScene(){
+		Application.LoadLevel("Pantalla Principal");
+	}
+}
+
+public partial class SceneChange : MonoBehaviour{
+
+	public void Exit(){
+		Application.LoadLevel("Inicio");
+	}
+}
+
+public partial class SceneChange : MonoBehaviour{
+
+	public void Camera(){
+		Application.LoadLevel("Camara");
+	}
+}

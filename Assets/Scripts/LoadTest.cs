d384781b014b35a644f575be31e2f859edb32986
Arquery <sgu_sigu@msn.com>
2019-07-03 15:59:33 -0600

    Charge Screen
    Was added a panelcharge together to your components.
    Was added a panelcharge in the "Pantalla principal" scene, called LoadPanel.
    Was created a script called LoadTest to managment for scene charge.

---
+++ Assets/Scripts/LoadTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadTest : MonoBehaviour {
+
+    public GameObject loadPanel;
+    public Slider loadBar;
+    public Text charge;
+
+    //private float percent;
+
+    private AsyncOperation asyn;
+
+    public void ClickCarga()
+    {
+        loadPanel.SetActive(true);
+        StartCoroutine(LoadSlider("Camara"));
+    }
+
+    IEnumerator LoadSlider(string scene)
+    {
+        asyn = Application.LoadLevelAsync("Camara");
+        while(!asyn.isDone)
+        {
+            loadBar.value = asyn.progress * 100;
+            charge.text = loadBar.value.ToString() + "%";
+            yield return null;
+        }
+    }
+
+}
